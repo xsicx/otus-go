03068cff9e0b3048a8f6a0cc22cb3b92cbfe5c46
Denis Ptushko <d.ptushko@gmail.com>
2022-03-01 23:43:15 +0530

    Complete HW1

--- hw01_hello_otus/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/fixme_my_friend/hw01_hello_otus
-
-go 1.16
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	s := "Hello, OTUS!"
+	s = stringutil.Reverse(s)
+	fmt.Printf("%s", s)
 }
