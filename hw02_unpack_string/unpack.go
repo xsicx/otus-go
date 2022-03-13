4eb1af6a16b2a06ed7062d234db5afe93e0db22e
Denis Ptushko <d.ptushko@gmail.com>
2022-03-13 20:10:38 +0530

    complete HW2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,75 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var strBuilder strings.Builder
+	lastSymbol, isEscaped := "", false
+
+	if !isCorrectString(str) {
+		return "", ErrInvalidString
+	}
+
+	for _, val := range str {
+		if val == '\\' && !isEscaped {
+			isEscaped = true
+			continue
+		}
+
+		if unicode.IsDigit(val) && !isEscaped {
+			if val == '0' {
+				removeLastSymbol(&strBuilder)
+				continue
+			}
+			size, _ := strconv.Atoi(string(val))
+			strBuilder.WriteString(strings.Repeat(lastSymbol, size-1))
+		} else {
+			strBuilder.WriteRune(val)
+			isEscaped = false
+		}
+
+		lastSymbol = string(val)
+	}
+
+	return strBuilder.String(), nil
+}
+
+func isCorrectString(str string) bool {
+	prevSymbolIsDigit, isEscaped := false, false
+
+	for i, val := range str {
+		if val == '\\' && !isEscaped {
+			isEscaped = true
+			continue
+		}
+
+		if unicode.IsDigit(val) && !isEscaped {
+			if i == 0 || prevSymbolIsDigit {
+				return false
+			}
+
+			prevSymbolIsDigit = true
+			continue
+		}
+
+		isEscaped = false
+		prevSymbolIsDigit = false
+	}
+
+	return true
+}
+
+func removeLastSymbol(builder *strings.Builder) {
+	str := builder.String()
+	builder.Reset()
+	if len(str) > 0 {
+		str = str[:len(str)-1]
+	}
+	builder.WriteString(str)
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,14 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a0", expected: ""},
+		{input: "a4a0", expected: "aaaa"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +37,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `\378`, `1\erwrwa4`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
