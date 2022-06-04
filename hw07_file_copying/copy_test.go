3fd5355df1bc74632e451017be29f784d5b93b48
Denis Ptushko <d.ptushko@gmail.com>
2022-06-04 16:49:11 +0300

    Complete HW7

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("unsupported file error", func(t *testing.T) {
+		fromPath := "broken111File"
+		toPath := "out.txt"
+		var offset, limit int64
+		err := Copy(fromPath, toPath, offset, limit)
+
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+	})
+
+	t.Run("offset error", func(t *testing.T) {
+		fromPath := "testdata/input2.txt" // 2 bytes
+		toPath := "out.txt"
+		var offset int64 = 100
+		var limit int64
+		err := Copy(fromPath, toPath, offset, limit)
+
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	})
 }
