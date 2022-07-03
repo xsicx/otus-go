3258acffe8e3c7818d62fbac613c3c5179c2ba59
Denis Ptushko <d.ptushko@gmail.com>
2022-07-03 19:02:34 +0300

    test(envdir): add tests
    HW-8

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -7,7 +7,7 @@ import (
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	c := exec.Command(cmd[0], cmd[1:]...)
+	c := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
